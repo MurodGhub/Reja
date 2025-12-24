09b007d2bdf7fbc5cc0a859315d31b629fd61802
MurodGhub <norqulovmurodbek07@gmail.com>
2025-12-24 15:59:43 -0500

    Build frontend with EJS framework

--- server.js
+++ server.js
@@ -15,12 +15,22 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing cocde
-app.get("/", function (req, res) {
-  res.end(`<h1 style="background: red">HELLO WORLD by Morris</h1>`);
+
+// app.get("/", function (req, res) {
+//   res.end(`<h1 style="background: red">HELLO WORLD by Morris</h1>`);
+// });
+
+// app.get("/gift", function (req, res) {
+//   res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+// });
+
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
 
-app.get("/gift", function (req, res) {
-  res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
