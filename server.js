f22f3e2047de564d91e7eca2e8b3b342957bf06d
MurodGhub <norqulovmurodbek07@gmail.com>
2025-12-25 21:17:29 -0500

    feat: Develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  }
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -27,6 +35,10 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
   console.log(req.body);
   res.json({ test: "success" });
+});
+
+app.get(`/author`, (req, res) => {
+  res.render("author", { user, user });
 });
 
 app.get("/", function (req, res) {
