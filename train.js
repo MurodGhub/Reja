44ddeb7f8402c257ca7d275ea19f9faf3d194d2d
MurodGhub <norqulovmurodbek07@gmail.com>
2025-12-29 21:50:59 -0500

    feat: train call back hamda async functions

--- train.js
+++ train.js
@@ -1,1 +1,160 @@
-console.log(`Train area`);
+console.log("Jack Ma maslahatlari");
+const list = [
+  "yaxshi talabboling", // 0-20
+  "togri boshliq tanlang va koproq xato qiling", // 20=30
+  "uzingizga ishlashni boshlang ", // 30-40
+  "siz kuchli bolgan narsalarni qiling", // 40-50
+  "yoshlarga investitsiya qiling", // 50-60
+  "endi dam oling", // 60
+];
+
+function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("insert a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[3]);
+  else if (a > 40 && a <= 50) callback(null, list[4]);
+  else if (a > 50 && a <= 60) callback(null, list[5]);
+  else {
+    // callback(null, list[5]);
+
+    setInterval(function () {
+      callback(null, list[5]);
+    }, 5000);
+  }
+}
+
+console.log("passed here 0");
+maslahatBering(65, (err, data) => {
+  if (err) console.log("ERROR:", err);
+  else {
+    console.log("javob:", data);
+  }
+});
+console.log("passed here 1");
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talabboling", // 0-20
+//   "togri boshliq tanlang va koproq xato qiling", // 20=30
+//   "uzingizga ishlashni boshlang ", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling", // 60
+// ];
+
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number", null);
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[3];
+//   else if (a > 40 && a <= 50) return list[4];
+//   else if (a > 50 && a <= 60) return list[5];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 5000);
+//     });
+//   }
+// }
+
+// async function run() {
+//   let javob = await maslahatBering(25);
+//   console.log(javob);
+//   javob = await maslahatBering(70);
+//   console.log(javob);
+//   javob = await maslahatBering(41);
+//   console.log(javob);
+// }
+// run();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talabboling", // 0-20
+//   "togri boshliq tanlang va koproq xato qiling", // 20=30
+//   "uzingizga ishlashni boshlang ", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling", // 60
+// ];
+
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number", null);
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[3];
+//   else if (a > 40 && a <= 50) return list[4];
+//   else if (a > 50 && a <= 60) return list[5];
+//   else {
+//     return list[5];
+//   }
+// }
+
+////// then catch:
+
+// console.log("passed here 0");
+// maslahatBering(25)
+//   .then((data) => {
+//     console.log("javob:", data);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR:", err);
+//   });
+// console.log("passed here 1");
+
+////// async/await
+
+// async function run() {
+//   let javob = await maslahatBering(20);
+//   console.log(javob);
+//   javob = await maslahatBering(31);
+//   console.log(javob);
+//   javob = await maslahatBering(41);
+//   console.log(javob);
+// }
+// run();
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talabboling", // 0-20
+//   "togri boshliq tanlang va koproq xato qiling", // 20=30
+//   "uzingizga ishlashni boshlang ", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling", // 60
+// ];
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[3]);
+//   else if (a > 40 && a <= 50) callback(null, list[4]);
+//   else if (a > 50 && a <= 60) callback(null, list[5]);
+//   else {
+//     // callback(null, list[5]);
+
+//     setTimeout(function () {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+
+// console.log("passed here 0");
+// maslahatBering(65, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log("javob:", data);
+//   }
+// });
+// console.log("passed here 1");
+
+// maslahatBering(39, (err, data) => {
+//   if (err) console.log(`ERROR:`, err);
+//   console.log(`javob:`, data);
+// });
