8fd4a274c5b74a1b7c814963a7c50e1db0111834
MurodGhub <norqulovmurodbek07@gmail.com>
2026-01-01 05:41:08 -0500

    MongoDB connection fixed

--- server.js
+++ server.js
@@ -1,35 +1,9 @@
-// const http = require("http");
-// const mongodb = require("mongodb");
-
-// const PORT = 3000;
-// let db;
-// const connectionString =
-//   "mongodb+srv://norqulovmurodbek07_db_user:lvxUDs1H8u4zGCYz@cluster0.dfpqzwj.mongodb.net/test";
-
-// mongodb.connect(connectionString, { useNewUrlParser: true }, (err, client) => {
-//   if (err) return console.log("MongoDB connection ERROR");
-
-//   db = client.db("test");
-//   console.log("MongoDB connection succed");
-
-//   const app = require("./views/app");
-//   const server = http.createServer(app);
-//   server.listen(PORT, () =>
-//     console.log(
-//       `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-//     )
-//   );
-// });
-
-// module.exports = {
-//   getDb: () => db,
-// };
 const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
 const connectionString =
-  "mongodb+srv://norqulovmurodbek07_db_user:lvxUDs1H8u4zGCYz@cluster0.dfpqzwj.mongodb.net/Reja?appName=Cluster0";
+  "mongodb+srv://norqulovmurodbek07_db_user:lvxUDs1H8u4zGCYz@cluster0.dfpqzwj.mongodb.net/Reja";
 
 mongodb.connect(
   connectionString,
