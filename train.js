cc75d2fe1d1927b22a593ca33e7db65458d997ae
MurodGhub <norqulovmurodbek07@gmail.com>
2026-01-03 00:49:31 -0500

    feat: MITtask Task C

--- train.js
+++ train.js
@@ -1,22 +1,76 @@
-/// MIT task: B-task (2)
+/// MIT task: Task C:
+/* Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud! */
 
-function countDigits(password) {
-  let numbers = 0;
-  let index = 0;
+// Solution for Task C
 
-  while (index < password.length) {
-    if (password[index] > 1 && password[index] <= 9) {
-      numbers++;
-    }
-    index++;
+class Wawa {
+  constructor(lays, sut, cola) {
+    this.lays = lays;
+    this.sut = sut;
+    this.cola = cola;
   }
 
-  return numbers;
+  getVaqt() {
+    const hozir = new Date();
+    const minut =
+      hozir.getMinutes() < 10 ? "0" + hozir.getMinutes() : hozir.getMinutes();
+    return `${hozir.getHours()}:${minut}`;
+  }
+
+  qoldiq() {
+    console.log(
+      `hozir ${this.getVaqt()}da ${this.lays}ta lays, ${this.sut}ta sut va ${
+        this.cola
+      }ta cola mavjud!`
+    );
+  }
+
+  sotish(nomi, soni) {
+    if (nomi === "lays") {
+      this.lays -= soni;
+    } else if (nomi === "sut") {
+      this.sut -= soni;
+    } else if (nomi === "cola") {
+      this.cola -= soni;
+    }
+    console.log(`${this.getVaqt()}da ${soni}ta ${nomi} sotildi`);
+  }
+
+  qabul(mahsulot, miqdor) {
+    if (mahsulot === "lays") this.lays += miqdor;
+    if (mahsulot === "sut") this.sut += miqdor;
+    if (mahsulot === "cola") this.cola += miqdor;
+
+    console.log(`${this.getVaqt()}da ${miqdor}ta ${mahsulot} qabul qilindi`);
+  }
 }
 
-console.log(countDigits("Pa$$w0rd_2026_Hacker99"));
+const myShop = new Wawa(2, 4, 3);
 
-/// MIT task: A-task (1)
+myShop.qoldiq();
+myShop.sotish("lays", 3);
+myShop.qabul("cola", 4);
+myShop.qoldiq();
+/// MIT task: Task B
+
+// function countDigits(password) {
+//   let numbers = 0;
+//   let index = 0;
+
+//   while (index < password.length) {
+//     if (password[index] > 1 && password[index] <= 9) {
+//       numbers++;
+//     }
+//     index++;
+//   }
+
+//   return numbers;
+// }
+
+// console.log(countDigits("Pa$$w0rd_2026_Hacker99"));
+
+/// MIT task: Task A
 
 // function countLetter(letter, word) {
 //   let count = 0;
